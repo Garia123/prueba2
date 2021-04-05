60aa285de1b8d296f1be5397aa61644695861bdb
Garia123 <44539954+Garia123@users.noreply.github.com>
2021-04-05 19:29:37 -0300

    prueba1

---
+++ BetterCalm/WebApplication/Controllers/FirstController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FirstController : ControllerBase
+    {
+        
+
+        public FirstController()
+        {
+
+        }
+
+    }
+}
